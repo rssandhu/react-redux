a808a7fa7d052a3c4ccabd9ed605f7e00905752a
InStep Technologies <sunder@insteptechnologies.com>
2018-06-11 12:50:12 +0530

    update in reducer switch case

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -5,28 +5,53 @@ const initialstate = {
 
 const reducer =(state = initialstate , action) => {
 
-	if(action.type === 'INCREMENT'){
-		return {
-			counter : state.counter + action.val
-		}
-	}
-	if(action.type === 'Decrement'){
-		return {
-			counter : state.counter - action.val 
-		}
-	}
-	if(action.type === 'Add5'){
-		return {
-			counter : state.counter + action.val 
-		}
-	}
-	if(action.type === 'Sub5'){
-		return {
-			counter : state.counter - action.val
-		}
+	switch(action.type){    //reducer with switch case
+
+		case 'INCREMENT' :
+			return {
+				counter : state.counter + action.val
+			}
+		case 'Decrement':
+			return {
+				counter : state.counter - action.val 
+			}
+		case 'Add5':
+			return {
+				counter : state.counter + action.val 
+			}
+		case 'Sub5':
+			return {
+				counter : state.counter - action.val
+			}
+
+		default : 
+			return state;
+
 	}
 
-	return state;
+
+	// if(action.type === 'INCREMENT'){      //reducer with if conditions 
+	// 	return {
+	// 		counter : state.counter + action.val
+	// 	}
+	// }
+	// if(action.type === 'Decrement'){
+	// 	return {
+	// 		counter : state.counter - action.val 
+	// 	}
+	// }
+	// if(action.type === 'Add5'){
+	// 	return {
+	// 		counter : state.counter + action.val 
+	// 	}
+	// }
+	// if(action.type === 'Sub5'){
+	// 	return {
+	// 		counter : state.counter - action.val
+	// 	}
+	// }
+
+	// return state;
 }
 
 
