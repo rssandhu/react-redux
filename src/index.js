ecdcca4cf33b1ed9f7fdf60ca5e1bbb226310689
InStep Technologies <sunder@insteptechnologies.com>
2018-06-11 15:36:05 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers } from 'redux';
 
-import {createStore} from 'redux';
-import reducer from  './store/reducer';
-import {Provider} from 'react-redux';
-
+import counterReducer from './store/reducers/counter';
+import resultReducer from './store/reducers/result';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(reducer);
+const rootReducer = combineReducers({
+    ctr: counterReducer,
+    res: resultReducer
+});
 
-ReactDOM.render(<Provider store = {store} ><App /></Provider>, document.getElementById('root'));
+const store = createStore(rootReducer);
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
